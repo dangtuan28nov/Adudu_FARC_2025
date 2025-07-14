3a3a086e6ab12b94fc4a4b14a1a56ddce4298fba
Dinh Anh Tuan Dang <antondang2811@gmail.com>
2025-07-15 00:33:32 +0700

    Add files via upload

---
+++ Lifting.h
@@ -0,0 +1,63 @@
+#pragma once
+
+namespace Lifting{
+  //Locations
+  constexpr int Up_1 =   12;
+  constexpr int Down_1 = 13;
+  constexpr int Up_2 =   10;
+  constexpr int Down_2 = 11;
+  //Specifications
+  constexpr int Speed_Lift =      4000;
+  constexpr int Speed_Down =      -200;
+  constexpr int Speed_Unchanged = 300; //The height of Lift is unchanged
+  constexpr int Speed_Climb =     -4050; //Speed of DC motor for climbing
+
+  bool Climb_Status = false;
+
+  //Blueprints for Lifting Motors
+  class Scuderia_Lift {
+    public:
+      void Update(){
+        if (ps2x.ButtonPressed(PSB_L2)) Climb_Status = !Climb_Status;
+
+        if (Climb_Status == true) {
+          Set(Speed_Climb);
+
+        } else if (Climb_Status == false) {
+          if (ps2x.Button(PSB_R1)) {
+            Set(Speed_Lift);
+            
+          } else if (ps2x.Button(PSB_R2)) {
+            Set(Speed_Down);
+
+          } else {
+            Set(0);
+          }
+        }
+      }
+    private:
+      void Set(int Speed){
+        if (Speed == 0) {
+          //Making sure the motor does not change its direction incontinently
+          pwm.setPWM (Up_1, 0, 0);
+          pwm.setPWM (Up_2, 0, 0);
+          pwm.setPWM (Down_1, 0, 0);
+          pwm.setPWM (Down_2, 0, 0);
+        } 
+        if (Speed > 0){
+          pwm.setPWM (Up_1, 0, Speed);
+          pwm.setPWM (Up_2, 0, Speed);
+        }
+        if (Speed < 0){
+          pwm.setPWM (Down_1, 0, abs(Speed));
+          pwm.setPWM (Down_2, 0, abs(Speed));
+        }
+      }
+  };
+}
+
+Lifting::Scuderia_Lift Leclerc; //Leclerc = Name of the Lifters
+
+inline void Lift(){
+  Leclerc.Update(); //Leclerc = Name of the Lifters
+}
